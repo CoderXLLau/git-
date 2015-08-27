5a6c86d09d26b3d7d3bf1246ef31699f557d21c3
Liushannoon <Shannoon@liuxianglong.local>
2015-08-27 18:27:25 +0800

    05--新人添加了一个控制器

---
+++ 03--github服务器做共享库/03--github服务器做共享库/adf ViewController.h
@@ -0,0 +1,13 @@
+//
+//  adf ViewController.h
+//  03--github服务器做共享库
+//
+//  Created by Liushannoon on 15/8/27.
+//  Copyright (c) 2015年 Liushannoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface adf_ViewController : UIViewController
+
+@end
---
+++ 03--github服务器做共享库/03--github服务器做共享库/adf ViewController.m
@@ -0,0 +1,37 @@
+//
+//  adf ViewController.m
+//  03--github服务器做共享库
+//
+//  Created by Liushannoon on 15/8/27.
+//  Copyright (c) 2015年 Liushannoon. All rights reserved.
+//
+
+#import "adf ViewController.h"
+
+@interface adf_ViewController ()
+
+@end
+
+@implementation adf_ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
