27fac847e3d24691dee302f70e52a517f274eecb
Liushannoon <Shannoon@liuxianglong.local>
2015-08-27 18:16:38 +0800

    01--初始化项目

---
+++ 03--github服务器做共享库/03--github服务器做共享库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03--github服务器做共享库
+//
+//  Created by Liushannoon on 15/8/27.
+//  Copyright (c) 2015年 Liushannoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03--github服务器做共享库/03--github服务器做共享库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03--github服务器做共享库
+//
+//  Created by Liushannoon on 15/8/27.
+//  Copyright (c) 2015年 Liushannoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
